78ebc26bfa8800a42b226f2d5ae3b67fe5d2e054
samnguyen02 <samnguyen02@tamu.edu>
2026-02-02 22:30:11 -0600

    week3 individual lab

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem "jbuilder"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
+gem "brakeman"
+gem "rubocop"
+
 gem "yaml_db"
 
 # Use the database-backed adapters for Rails.cache, Active Job, and Action Cable
@@ -32,6 +35,9 @@ gem "solid_cable"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+
+# Required for Ruby 3+ XML parsing
+gem "rexml"
 
 # Deploy this application anywhere as a Docker container [https://kamal-deploy.org]
 gem "kamal", require: false
@@ -46,9 +52,6 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
 
-  # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
-  gem "brakeman", require: false
-
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
 
@@ -57,7 +60,6 @@ group :development, :test do
   gem "factory_bot_rails"
 end
 
-gem "rubocop"
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,51 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# Clear existing data to avoid duplicates when re-seeding
+Book.destroy_all
+Author.destroy_all
+User.destroy_all
+UserBook.destroy_all
+
+# Create Authors
+authors = Author.create!([
+  { name: "J.K. Rowling" },
+  { name: "George Orwell" },
+  { name: "Harper Lee" },
+  { name: "F. Scott Fitzgerald" },
+  { name: "Jane Austen" }
+])
+
+# Create Books (at least 5 entries as required)
+books = Book.create!([
+  { title: "Harry Potter and the Sorcerer's Stone", price: 19.99, published_date: Date.new(1997, 6, 26), author: authors[0] },
+  { title: "1984", price: 14.99, published_date: Date.new(1949, 6, 8), author: authors[1] },
+  { title: "To Kill a Mockingbird", price: 12.99, published_date: Date.new(1960, 7, 11), author: authors[2] },
+  { title: "The Great Gatsby", price: 11.99, published_date: Date.new(1925, 4, 10), author: authors[3] },
+  { title: "Pride and Prejudice", price: 9.99, published_date: Date.new(1813, 1, 28), author: authors[4] },
+  { title: "Animal Farm", price: 10.99, published_date: Date.new(1945, 8, 17), author: authors[1] },
+  { title: "Harry Potter and the Chamber of Secrets", price: 19.99, published_date: Date.new(1998, 7, 2), author: authors[0] }
+])
+
+# Create Users
+users = User.create!([
+  { username: "bookworm123" },
+  { username: "reader_jane" },
+  { username: "library_lover" }
+])
+
+# Create UserBooks relationships
+UserBook.create!([
+  { user: users[0], book: books[0] },
+  { user: users[0], book: books[1] },
+  { user: users[1], book: books[2] },
+  { user: users[1], book: books[3] },
+  { user: users[2], book: books[4] }
+])
+
+puts "Seed data created successfully!"
+puts "Created #{Author.count} authors"
+puts "Created #{Book.count} books"
+puts "Created #{User.count} users"
+puts "Created #{UserBook.count} user-book relationships"
